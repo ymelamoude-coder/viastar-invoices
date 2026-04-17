9bd1b71433ae0ae1c3314ff1c1f46528c4f33227
ymelamoude-coder <y.melamoude@gmail.com>
2026-04-17 14:10:17 -0400

    Add files via upload

--- api/parse-pdf.js
+++ api/parse-pdf.js
@@ -1,3 +1,11 @@
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb'
+    }
+  }
+}
+
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
